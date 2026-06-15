c365981ff549418dcdfc2e139c6e02c813f2c173
malini132011-mk <malini132011@gmail.com>
2026-06-15 18:43:13 +0530

    Add files via upload

---
+++ NumberGame.java
@@ -0,0 +1,52 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class NumberGame {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Random random = new Random();
+
+        int score = 0;
+        String playAgain;
+
+        do {
+            int number = random.nextInt(100) + 1;
+            int attempts = 5;
+            boolean guessed = false;
+
+            System.out.println("Guess the Number (1 to 100)");
+
+            while (attempts > 0) {
+                System.out.print("Enter your guess: ");
+                int guess = sc.nextInt();
+
+                if (guess == number) {
+                    System.out.println("Correct! You guessed the number.");
+                    score += attempts;
+                    guessed = true;
+                    break;
+                } else if (guess < number) {
+                    System.out.println("Too Low!");
+                } else {
+                    System.out.println("Too High!");
+                }
+
+                attempts--;
+                System.out.println("Attempts left: " + attempts);
+            }
+
+            if (!guessed) {
+                System.out.println("Game Over! The number was " + number);
+            }
+
+            System.out.println("Score: " + score);
+
+            System.out.print("Play Again? (yes/no): ");
+            playAgain = sc.next();
+
+        } while (playAgain.equalsIgnoreCase("yes"));
+
+        System.out.println("Final Score: " + score);
+        sc.close();
+    }
+}
